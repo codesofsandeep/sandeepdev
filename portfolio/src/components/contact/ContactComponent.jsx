a2e661846cff63eb49795500d15a07a70dff4464
sandeep rajput <sandeeprajput@Mac.lan>
2025-12-02 23:25:09 +0530

    Reponsive Fix

--- portfolio/src/components/contact/ContactComponent.jsx
+++ portfolio/src/components/contact/ContactComponent.jsx
@@ -68,7 +68,8 @@ const ContactComponent = () => {
         </motion.div>
 
         {/* Main Content Grid */}
-        <div className="grid md:grid-cols-2 gap-8">
+        {/* <div className="grid md:grid-cols-2 gap-8"> */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 w-full">
 
           {/* Email Section */}
           <motion.div
